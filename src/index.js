4aca488798fa7ad72a3b3dfa2784348c7cbf527e
olia123456 <88795985+olia123456@users.noreply.github.com>
2023-01-02 19:01:39 +0000

    feat: "end task"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let a = String(n);
+    let b = "";
+    let l = a.length;
+    for (let i = 0; i < l; i++) {
+        b += a[l - 1 - i];
+    }
+    return parseInt(b);
 }
